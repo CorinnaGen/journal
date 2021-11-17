c7c455ae34168b35445f2917918dfd707d6df28e
Corinna-Genchi <corinnagenchi@gmail.com>
2021-11-17 18:03:27 +0100

    add logic to tracker

--- client/src/components/Tracker.js
+++ client/src/components/Tracker.js
@@ -1,7 +1,56 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
 function Tracker() {
     const [sumMood, setSumMood] = useState([]);
+    const [entries, setEntries] = useState([])
+    const [error, setError] = useState([])
+
+//fetch the entries
+// useEffect(getEntries, []);
+
+// const getEntries = async () =>{
+//     try {
+// 			const response = await fetch(`/journal_entries`);
+// 			const data = await response.json();
+// 			setEntries(data);
+// 		} catch (err) {
+// 			setError(err);
+// 		}
+// }
+
+    //to generate a mood value
+const generateMoodValue = () => {
+const sumMood = 0;
+setEntries.map(entry => {
+    switch(entry.mood){
+        case 'Happy' :
+        sumMood += 120; 
+            break;
+        case 'Hopeful' :
+        sumMood += 100; 
+            break;
+        case 'Optimistic' :
+        sumMood += 110; 
+            break;
+        case 'Tired' :
+        sumMood += 60; 
+            break;
+        case 'Anxious' :
+        sumMood += 60; 
+            break;
+        case 'Disconnected' :
+        sumMood += 30; 
+            break;
+            case 'Sad' :
+        sumMood -= 120; 
+            break;
+        case 'Depressed' :
+        sumMood -= 120; 
+            break;
+    }
+    
+})
+}
 
 //display mood of each day
 //having a function assign a weight to each mood
