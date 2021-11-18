91cbdf3b28df2286f1fc5a94a683b0beac14c29c
Corinna-Genchi <corinnagenchi@gmail.com>
2021-11-18 16:59:53 +0100

    moodtrack done

--- client/src/components/Tracker.js
+++ client/src/components/Tracker.js
@@ -5,9 +5,10 @@ function Tracker() {
    
     const [entries, setEntries] = useState([]) //this is an array of obj
     const [error, setError] = useState([])
+ 
     
 
-	useEffect(async () => {
+useEffect(async () => {
 		try {
 			const response = await fetch("/journal_entries");
 			const data = await response.json();
@@ -24,52 +25,69 @@ function Tracker() {
 const sumMood = entries.map(entry => {
     switch(entry.mood){
         case 'Happy' :
-        return 'green'
+        return 'happy'
         case 'Hopeful' :
-        return 'light-green'
+        return 'hopeful'
         case 'Optimistic' :
-        return 'light-blue'
+        return 'optmistic'
         case 'Tired' :
-        return 'grey'
+        return 'tired'
         case 'Anxious' :
-        return 'yellow'
+        return 'anxious'
         case 'Disconnected' :
-        return 'orange'
+        return 'disconnected'
          case 'Angry' :
-        return 'red'
+        return 'angry'
             case 'Sad' :
-        return 'purple'
+        return 'sad'
         case 'Depressed' :
-        return 'black'
+        return 'depressed'
             
     }
  
 });
 console.log(entries)
 console.log(sumMood)
+
+//to display the prevalent mood among the entries
+
+let prevalentMood = sumMood.reduce((counter, current) => (counter[current] = counter[current] + 1 || 1, counter), {});
+//it returns an object like this {tired: 1, angry: 1, sad: 1, depressed: 1, happy: 1}
+let moodTrack = [];
+for(let key in prevalentMood){
+if(prevalentMood[key] > 1){
+ moodTrack.push(key)
+}}
+
+console.log(moodTrack)
   
 
     return (
         <div className="container bg-light shadow mt-4">
             <h3 className="darker"> Tracker</h3>
             <p>Your mood in the past days based on your entries</p>
+            {moodTrack.map((el, i) => (
+                <div key={i}>
+                <p>You felt mostly: {el}</p>
+                </div>
+            ))}
             <div className="row m-4">
             {sumMood.map((mood, i) => (
                             <div className="col-3" key={i}>
-                            {(mood === 'green') ? (<div className="mood green"></div>)
-                            : (mood ==='light-green') ?
-                            (<div className=" mood light-green"></div>)
-                            : (mood === 'light-blue') ? (<div className="mood light-blue"></div>)
-                            : (mood === 'grey') ? (<div className="mood grey"></div>)
-                            : (mood === 'yellow') ? (<div className=" mood yellow"></div>)
-                            : (mood === 'orange') ? (<div className="mood orange"></div>)
-                            : (mood ==='red') ? (<div className="mood red"></div>)
-                            : (mood === 'purple') ? (<div className="mood purple"></div>)
-                            : (<div className="mood black"></div>)}  
+                            {(mood === 'happy') ? (<div className="mood happy"></div>)
+                            : (mood ==='hopeful') ?
+                            (<div className=" mood hopeful"></div>)
+                            : (mood === 'optimistic') ? (<div className="mood optimistic"></div>)
+                            : (mood === 'tired') ? (<div className="mood tired"></div>)
+                            : (mood === 'anxious') ? (<div className=" mood anxious"></div>)
+                            : (mood === 'disconnected') ? (<div className="mood disconnected"></div>)
+                            : (mood ==='angry') ? (<div className="mood angry"></div>)
+                            : (mood === 'sad') ? (<div className="mood sad"></div>)
+                            : (<div className="mood depressed"></div>)}  
                             </div>
                     
               
-            ))}  
+            ))} 
             </div>
             
         </div>
