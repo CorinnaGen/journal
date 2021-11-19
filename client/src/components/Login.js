fd2a7b61e81948292340a2e9c15f71508268b851
Corinna-Genchi <corinnagenchi@gmail.com>
2021-11-19 17:28:02 +0100

    really working now

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -11,15 +11,19 @@ function Login() {
   const { username, password } = credentials;
 
   const handleChange = (e) => {
+    e.preventDefault();
     const { name, value } = e.target;
     setCredentials({ ...credentials, [name]: value });
   };
 
   const login = async () => {
+
+      console.log("am i in the login?")
     try {
       const { data } = await axios("/users/login", {
         method: "POST",
         data: credentials,
+        
       });
 
       //store it locally
@@ -30,15 +34,16 @@ function Login() {
     }
   };
 
-//   const logout = () => {
-//     localStorage.removeItem("token");
-//   };
+  const logout = () => {
+    localStorage.removeItem("token");
+    console.log('am i in the logout?')
+  };
 
   const requestData = async () => {
     try {
       const { data } = await axios("/journal_entries", {
         headers: {
-          authorization: "Bearer " + localStorage.getItem("token"),
+          authorization: `Bearer ${localStorage.getItem("token")}`,
         },
       });
 
@@ -56,18 +61,21 @@ function Login() {
                 <h2 className='darker'>Welcome back!</h2>
                 <br />
             <h3 className='darker'>Login</h3>
-            <form onSubmit={login} >
+            
                 <div className="col-6">
             <label>Username</label>
             <input type="text" name="username" placeholder="type your username" value={username} className="form-control" onChange={(e)=>handleChange(e)}/>
             <label>Password</label>
             <input type="password" name="password" className="form-control" value={password} onChange={(e)=>handleChange(e)}/>
-            <button className="btn btn-test6 bg-test6 m-4">Log in</button>
+            <button className="btn btn-test6 bg-test6 m-4" type="submit" onClick={login}>Log in</button>
+           
             <div className="col-6 mt-4">
             <a href="#"> I forgot my password!</a>
             </div>
             </div>
-            </form>
+            
+             <button className="btn btn-test6 bg-test6 m-4" onClick={logout}>Log out</button>
+              <button className="btn btn-test6 bg-test6 m-4" onClick={requestData}>see entries</button>
         </div>
     )
 }
