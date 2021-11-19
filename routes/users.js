dd5ba7ee87fdf4e4d67c3c23ae57b269f0ef3b54
Corinna-Genchi <corinnagenchi@gmail.com>
2021-11-19 12:59:53 +0100

    login route done and tested

--- routes/users.js
+++ routes/users.js
@@ -28,9 +28,28 @@ router.post("/register", async (req, res) => {
 });
 
 //LOGIN ENDPOINT
+router.post("/login", async(req,res)=>{
+  const {username, password} = req.body;
 
+  try{
+    const results= await db(`SELECT * FROM users WHERE username = "${username}"`);
+    const user = results.data[0];
 
+    if (user) {
+      const user_id = user.id;
 
+      const correctPsw = await bcrypt.compare(password, user.password);
+
+      if(!correctPsw) throw new Error("Incorrect password, try again");
+      var token = jwt.sign({ user_id }, supersecret);
+      res.send({ message: "Login successful, here is your token", token });
+    } else {
+      throw new Error("User does not exist");
+    }
+  } catch (err) {
+    res.status(400).send({ message: err.message });
+  }
+});
 
 
 router.get('/', function(req, res, next) {
